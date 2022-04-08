df9526da2014e970a0536626f9beefaf8f0045f9
Atharva Pakade <77790083+atharva-310@users.noreply.github.com>
2022-04-08 10:46:30 +0530

    bugs in the vote route

--- app/routers/vote.py
+++ app/routers/vote.py
@@ -15,13 +15,13 @@ def vote(vote: schemas.Vote, db: Session = Depends(get_db), current_user: int = 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, details=f"post with id {vote.post_id} not found")
    
-    vote_query = db.query(models.Vote).filter(models.Vote.post_id == vote.id, models.Vote.user_id == current_user.id)
+    vote_query = db.query(models.Votes).filter(models.Votes.post_id == vote.id, models.Votes.user_id == current_user.id)
     found_vote = vote_query.first()
 
     if (vote.dir == 1):
         if found_vote:
             raise HTTPException(status_code=status.HTTP_409_CONFLICT, details=f"User {current_user.id} has already voted on the post")
-        new_vote = models.Vote(post_id = vote.post_id, user_id = vote.user_id)
+        new_vote = models.Votes(post_id = vote.post_id, user_id = vote.user_id)
         db.add(new_vote)
         db.commit()
         return {"message": "Vote successfully Added"}
